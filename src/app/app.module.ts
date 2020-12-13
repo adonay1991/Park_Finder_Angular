1c2be878636dfd90dedaf08828f46fb39bcdb7aa
Adonay Rodriguez <adonaydaniel16@gmail.com>
2020-12-13 23:24:53 +0100

    practica angular prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CouponsComponent } from './coupons/coupons.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CouponsComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/assets/coupons.ts
@@ -0,0 +1,32 @@
+export let cupones = [
+  {
+    imgSource: '../../assets/img/coupon-1.jpg',
+    textCoupon: 'First registration free',
+    textBtn: 'See coupon',
+  },
+  {
+    imgSource: '../../assets/img/coupon-2.jpg',
+    textCoupon: 'Black Friday',
+    textBtn: 'See coupon',
+  },
+  {
+    imgSource: '../../assets/img/coupon-3.jpg',
+    textCoupon: 'Flash Offer Friday',
+    textBtn: 'See coupon',
+  },
+  {
+    imgSource: '../../assets/img/coupon-4.jpg',
+    textCoupon: 'Vouncher Free!!!!!',
+    textBtn: 'See coupon',
+  },
+  {
+    imgSource: '../../assets/img/coupon-5.jpg',
+    textCoupon: 'With your big sale get a discount',
+    textBtn: 'See coupon',
+  },
+  {
+    imgSource: '../../assets/img/coupon-6.jpg',
+    textCoupon: 'Run for your buy on Black Friday',
+    textBtn: 'See coupon',
+  },
+];
